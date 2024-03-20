9720ce874cbe68ba6e1b52a3dcb28598521c211d
GiorgGogoladze <giogogoladze2010@gmail.com>
2024-03-20 18:39:55 +0400

    Fifth commit

---
+++ day5/lesson5.py
@@ -0,0 +1,9 @@
+salary = input("what is your salary?")
+if salary > 10000:
+    print("გოაში სწავლობდი?")
+elif salary > 1000 < 10000:
+    print("you are mid")
+elif salary < 1000:
+    print("შემოსულიყავი გოაში, მატრიცელო")
+else:
+    print("error")
