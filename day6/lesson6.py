8e48169554f30dff20a317f2eaba6ad41b64b2c5
GiorgGogoladze <giogogoladze2010@gmail.com>
2024-03-20 18:43:04 +0400

    sixth commit

---
+++ day6/lesson6.py
@@ -0,0 +1,12 @@
+dayli_routine = ("coding,coding and CODING ALL DAY")
+for i in range(7):
+    print(dayli_routine)
+
+#2nd
+i = 1
+while i > 30:
+    if i % 2:
+        print(i) + "even"
+    else:
+        print(str(i)) + "odd"
+        i=i+1
