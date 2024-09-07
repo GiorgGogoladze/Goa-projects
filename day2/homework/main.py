cb01b4810f2882a7687a76b840ae650ce3d73041
GiorgGogoladze <164012768+GiorgGogoladze@users.noreply.github.com>
2024-09-07 00:54:22 -0700

    commit

---
+++ day2/homework/main.py
@@ -0,0 +1,9 @@
+your_age = 13
+father_age = 38
+
+your_age_in_10_years = your_age + 10
+father_age_in_10_years = father_age + 10
+
+times_older = father_age_in_10_years / your_age_in_10_years
+
+print(f"10 წლის შემდეგ თქვენი მამის ასაკი თქვენი ასაკთან შედარებით გაიზრდება {times_older:.2f}-ჯერ.")
