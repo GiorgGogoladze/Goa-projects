dd23539d3ee2ff3ca013d0e0c2c2a9310ae8af95
GiorgGogoladze <164012768+GiorgGogoladze@users.noreply.github.com>
2024-09-07 00:58:04 -0700

    commit

---
+++ day04/homework/main.py
@@ -0,0 +1,14 @@
+# ოჯახის წევრების ასაკების ცვლადები
+your_age = 13
+father_age = 38
+mother_age = 39
+
+# განახლება 20 წლის შემდეგ
+your_age_in_20_years = your_age + 20
+father_age_in_20_years = father_age + 20
+mother_age_in_20_years = mother_age + 20
+
+# დაპრინტვა განახლებული ასაკების
+print(f"თქვენი ასაკი 20 წლის შემდეგ: {your_age_in_20_years}")
+print(f"მამის ასაკი 20 წლის შემდეგ: {father_age_in_20_years}")
+print(f"დედის ასაკი 20 წლის შემდეგ: {mother_age_in_20_years}")
