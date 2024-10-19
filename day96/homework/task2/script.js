9d80d3fe97684bc87b252b23a32ef81f217e08e0
GiorgGogoladze <164012768+GiorgGogoladze@users.noreply.github.com>
2024-10-19 13:38:34 +0400

    .

--- day96/homework/task2/script.js
+++
@@ -1,6 +0,0 @@
-const myMap = new Map();
-myMap.set('name', 'gio');
-myMap.set('age', 13);
-myMap.set('country', 'Georgia');
-
-console.log(myMap);
--- day96/homework/task3/script.js
+++
@@ -1,9 +0,0 @@
-const anotherMap = new Map();
-anotherMap.set('name', 'Alice');
-anotherMap.set('age', 25);
-anotherMap.set('job', 'Developer');
-
-console.log(anotherMap); 
-
-anotherMap.delete('age');
-console.log(anotherMap); 
